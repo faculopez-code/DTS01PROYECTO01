374c545f7f9c885a3a8c6c3e67e5dc0cd555582a
Facu Lopez <103524123+faculopez-code@users.noreply.github.com>
2022-12-09 05:31:20 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
+#importamos librerias a utilizar
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from datasets_etl import get_max_duration, get_count_platform, get_listedin, get_actor
 
+#creamos FastApi
 app = FastAPI()
 
+#mensaje de bienvenida con instrucciones para iniciar /docs
 @app.get('/', response_class=HTMLResponse)
 async def index():
     return '''
@@ -16,21 +19,22 @@ async def index():
     </body>
     </html>'''
 
-
+#retorna la maxima duración en minutos o temporadas de una pelicula/serie
 @app.get('/get_max_duration({año}_{plataforma}_{Movie_o_TV_Show})')
 async def duracion_maxima(año:int, plataforma:str, Movie_o_TV_Show:str):
     return get_max_duration(año, plataforma, Movie_o_TV_Show)
 
-
+#cuenta la cantidad de series y peliculas de una plataforma
 @app.get('/get_count_plataform({plataforma})')
 async def cantidad_plataforma(plataforma:str):
     return get_count_platform(plataforma)
 
-
+# Cantidad de veces que se repite un género y plataforma con mayor frecuencia del mismo.
 @app.get('/get_listedin({genero})')
 async def genero_plataforma(genero:str):
     return get_listedin(genero)
 
+#busca el actor mas frecuente
 @app.get('/get_actor({plataforma}_{anio})')
 async def actor_repetido(plataforma:str, anio:int):
     return get_actor(plataforma, anio)
